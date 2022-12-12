5a96c650e8c3efdadf71bc268b1c81fc2acb10a2
Sandeep <skumar8work@gmail.com>
2022-12-12 17:49:12 +0530

    📦 NEW: if else use

---
+++ script-2.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "Enter a value to comopare.."
+read 
+if [ $1 == 1 ]
+then
+	# if true print this
+	echo "Value entered is 1."
+else 
+	# else print this
+	echo "Value entered is not 1."
+fi
